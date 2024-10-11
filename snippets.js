3d41521f818e3f00b0e6bead53b60a7b8f14cc66
kocatekin <116370462+kocatekin@users.noreply.github.com>
2024-10-11 18:49:00 +0300

    added titlecae

--- snippets.js
+++ snippets.js
@@ -24,3 +24,15 @@ function trim(arr) {
             }
             return newArr;
         }
+
+//turns the first char of the str into uppercase 
+function titleCase(str) {
+    let myArr = str.split(" ");
+    let outputString = "";
+    for(let i=0;i<myArr.length;i++) {
+        outputString += myArr[i][0].toUpperCase()+ myArr[i].slice(1);
+        if(i == myArr.length - 1){break;}
+            outputString += " ";
+    }
+    return outputString;
+}
